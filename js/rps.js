54521bd74767f57681e735549e5b195df588a06d
Tim <275452338+timjebb-tech@users.noreply.github.com>
2026-06-17 15:27:48 +0100

    Single round now seems to be working

---
+++ js/rps.js
@@ -0,0 +1,81 @@
+
+
+
+const helloWorld = function() {
+  return 'Hello, World!'
+};
+
+const getComputerChoice = function() {
+    var choice = Math.floor(Math.random() *3);
+
+    switch (choice) {
+        case 0:
+            return("rock");
+            break;
+        case 1:
+            return("paper");
+            break;
+        case 2:
+            return("scissors");
+            break;
+        default:
+            throw Error("Unexpected random value generated");
+    }
+}
+
+const getHumanChoice = function() {
+    var notFinished = true;
+    var extratext = "";
+    while(notFinished)
+    {
+        value = prompt(`${extratext}Enter Rock, Paper, or Scissors`);
+        value = value.toLowerCase();
+        if (["rock", "paper", "scissors"].includes(value)) {
+            return value;
+        } 
+        extratext = `Invalid input: ${value} `;
+    }
+
+}
+
+const playRound = function(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log("It's a draw");
+    }
+    else if (humanChoice == "rock") {
+        if (computerChoice == "scissors") {
+            console.log("You win! Rock smashes scissors!");
+            humanScore++;
+        }
+        else {
+            console.log("Computer wins! Paper covers rock");
+            computerScore++;
+        }
+    }
+    else if (humanChoice == "scissors") {
+        if (computerChoice == "rock") {
+            console.log("Computer wins! Rock smashes scissors!");
+            computerScore++;
+        }
+        else {
+            console.log("You win! Scissors cut paper!");
+            humanScore++;
+        }
+    }
+    else { // paper
+        if (computerChoice == "rock") { 
+            console.log("You win! Paper covers rock");
+            humanScore++;
+        }
+        else {
+            console.log("Computer wins! Scissors cut paper!");
+            computerScore++;
+        }
+    }
+}
+var humanScore = 0;
+var computerScore = 0;
+var computerChoice = getComputerChoice();
+var humanChoice = getHumanChoice();
+
+playRound(humanChoice, computerChoice);
