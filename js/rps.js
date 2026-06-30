e66993a036d7cf49d7af781db1eb032f91e06160
Tim <275452338+timjebb-tech@users.noreply.github.com>
2026-06-30 12:12:54 +0100

    tweaks

--- js/rps.js
+++ js/rps.js
@@ -99,3 +99,10 @@ var humanScore = 0;
 var computerScore = 0;
 
 playGame(5);
+
+
+let arr = ["t", "e", "s", "t"];
+
+console.log( arr.slice(1, 3) ); // e,s (copy from 1 to 3)
+
+console.log( arr.slice(-2) ); // s,t (copy from -2 till the end)
