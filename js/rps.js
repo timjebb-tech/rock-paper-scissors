061be6af602a2d61cbfdca51ca81675066ee5075
Tim <275452338+timjebb-tech@users.noreply.github.com>
2026-07-01 13:29:09 +0100

    GUI driven game now seems to be working

--- js/rps.js
+++ js/rps.js
@@ -38,71 +38,115 @@ const getHumanChoice = function() {
 
 }
 
-const playRound = function(humanChoice, computerChoice) {
+const playRound = function() {
+    var computerChoice = getComputerChoice();
+    document.querySelector("#computerChoice").textContent = computerChoice;
     if (humanChoice == computerChoice) {
-        console.log("It's a draw");
+        updateResult("It's a draw");
     }
     else if (humanChoice == "rock") {
         if (computerChoice == "scissors") {
-            console.log("You win! Rock smashes scissors!");
+            updateResult("You win! Rock smashes scissors!");
             humanScore++;
         }
         else {
-            console.log("Computer wins! Paper covers rock");
+            updateResult("Computer wins! Paper covers rock");
             computerScore++;
         }
     }
     else if (humanChoice == "scissors") {
         if (computerChoice == "rock") {
-            console.log("Computer wins! Rock smashes scissors!");
+            updateResult("Computer wins! Rock smashes scissors!");
             computerScore++;
         }
         else {
-            console.log("You win! Scissors cut paper!");
+            updateResult("You win! Scissors cut paper!");
             humanScore++;
         }
     }
-    else { // paper
+    else if (humanChoice == "paper") {
         if (computerChoice == "rock") { 
-            console.log("You win! Paper covers rock");
+            updateResult("You win! Paper covers rock");
             humanScore++;
         }
         else {
-            console.log("Computer wins! Scissors cut paper!");
+            updateResult("Computer wins! Scissors cut paper!");
             computerScore++;
         }
     }
+    else
+    {
+        alert("please choose rock, paper or scissors");
+    }
+    
 }
 
-const playGame = function(roundCount) {
-    for (var i=0; i < roundCount; i++) {
-        var computerChoice = getComputerChoice();
-        var humanChoice = getHumanChoice();
+function updateScoreDisplay() {
+    document.querySelector("#score").textContent = `You: ${humanScore}, Computer: ${computerScore}, rounds played: ${roundCount}`;
+}
 
-        playRound(humanChoice, computerChoice);
-    }
+function updateResult(resultText) {
 
-    if (humanScore > computerScore)
-    {
-        console.log(`Human wins the contest ${humanScore} to ${computerScore}!`);
+    if (roundCount == 0) {
+        document.querySelector("#gameOver").textContent = "";
     }
-    else if (humanScore < computerScore)
+    roundCount++;
+    
+    updateScoreDisplay();
+    document.querySelector("#result").textContent = resultText;
+
+    if (roundCount >= 5)
     {
-        console.log(`Computer wins the contest ${computerScore} to ${humanScore}!`);
-    }
-    else {
-        console.log(`Contest is a draw!  ${humanScore} : ${computerScore}`);
+        if (humanScore > computerScore) {
+            document.querySelector("#gameOver").textContent = `Game Over!  You win! ${humanScore} : ${computerScore}`;
+        }
+        else if (humanScore < computerScore) {
+            document.querySelector("#gameOver").textContent = `Game Over!  Computer wins! ${computerScore} : ${humanScore}`;
+        }
+        else {
+            document.querySelector("#gameOver").textContent = `Game Over!  It's a draw! ${computerScore} : ${humanScore}`;
+        }
+        humanScore = 0;
+        computerScore = 0;
+        roundCount = 0;
     }
 }
+
+
+
+// const playGame = function(roundCount) {
+//     for (var i=0; i < roundCount; i++) {
+//         var computerChoice = getComputerChoice();
+//         var humanChoice = getHumanChoice();
+
+//         playRound(humanChoice, computerChoice);
+//     }
+
+//     if (humanScore > computerScore)
+//     {
+//         console.log(`Human wins the contest ${humanScore} to ${computerScore}!`);
+//     }
+//     else if (humanScore < computerScore)
+//     {
+//         console.log(`Computer wins the contest ${computerScore} to ${humanScore}!`);
+//     }
+//     else {
+//         console.log(`Contest is a draw!  ${humanScore} : ${computerScore}`);
+//     }
+// }
+
+var roundCount = 0;
+var humanChoice = "None"
+var computerScore = 0;
+var humanScore = 0;
 
 var elementsArray = document.querySelectorAll('.chooser');
-
-console.log(`elementsArray = ${elementsArray[0]}`);
 
 onChoose = function(e) {
     console.log("wibble");
     console.log(e.srcElement.id);
-    document.querySelector("#choice").textContent = e.srcElement.id;
+    document.querySelector("#yourChoice").textContent = e.srcElement.id;
+    humanChoice = e.srcElement.id;
 
 }
 
@@ -110,3 +154,5 @@ elementsArray.forEach(function(element) {
     element.addEventListener("click", onChoose);
     }
 );
+
+document.querySelector('#playRound').addEventListener("click", playRound);;
